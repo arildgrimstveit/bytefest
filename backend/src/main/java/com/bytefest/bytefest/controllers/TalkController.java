da21577c4f865c333d07a72ff296780b0c1bcdc8
Zubeyr MUSTAFA AHMED <zubeyr.mustafa@soprasteria.com>
2025-02-13 16:22:20 +0100

    Added talk class

---
+++ backend/src/main/java/com/bytefest/bytefest/controllers/TalkController.java
@@ -0,0 +1,85 @@
+package com.bytefest.bytefest.controllers;
+
+import com.bytefest.bytefest.models.Talk;
+import com.bytefest.bytefest.services.TalkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/talk/v2")
+@RequiredArgsConstructor
+@Validated
+public class TalkController {
+
+    private final TalkService talkService;
+
+    /**
+     * This method is called when a GET request is made
+     * URL: localhost:8080/talk/v1/
+     * Purpose: Fetches all the talks in the talk table
+     * @return List of Talks
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<Talk>> getAllTalks(){
+        return ResponseEntity.ok().body(talkService.getAllTalks());
+    }
+
+    /**
+     * This method is called when a GET request is made
+     * URL: localhost:8080/Talk/v1/{id}
+     * Purpose: Fetches employee with the given id
+     * @param id - user id
+     * @return Talk with the given id
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Talk> getUserById(@PathVariable Integer id)
+    {
+        return ResponseEntity.ok().body(talkService.getTalkById(id));
+    }
+
+    /**
+     * This method is called when a POST request is made
+     * URL: localhost:8080/Talk/v1/
+     * Purpose: Save a Talk
+     * @param talk - Request body is a Talk
+     * @return Saved Talk
+     */
+    @PostMapping("/")
+    public ResponseEntity<Talk> saveTalk(@RequestBody Talk talk)
+    {
+        return ResponseEntity.ok().body(talkService.saveTalk(talk));
+    }
+
+    /**
+     * This method is called when a PUT request is made
+     * URL: localhost:8080/Talk/v1/
+     * Purpose: Update a Talk
+     * @param talk - Talk to be updated
+     * @return Updated User
+     */
+    @PutMapping("/")
+    public ResponseEntity<Talk> updateUser(@RequestBody Talk talk)
+    {
+        return ResponseEntity.ok().body(talkService.updateTalk(talk));
+    }
+
+    /**
+     * This method is called when a PUT request is made
+     * URL: localhost:8080/Talk/v1/{id}
+     * Purpose: Delete a Talk
+     * @param id - talks's id to be deleted
+     * @return a String message indicating talk record has been deleted successfully
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Integer id)
+    {
+        talkService.deleteUserById(id);
+        return ResponseEntity.ok().body("Deleted talk successfully");
+    }
+
+
+}
---
+++ backend/src/main/java/com/bytefest/bytefest/models/Talk.java
@@ -0,0 +1,34 @@
+package com.bytefest.bytefest.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "talk")
+public class Talk {
+
+    @Id
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDate date;
+    private String room;
+    private Integer attendees;
+    private boolean isPublic;
+    private ContentType contentType;
+    private boolean isExternal;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public enum ContentType {
+        Java, Dot_Net, React, Design, Frontend, Other
+    }
+}
