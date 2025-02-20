25b702b185ac399262aadef2551a0f98971500cb
GRIMSTVEIT Arild <arild.grimstveit@soprasteria.com>
2025-02-20 13:35:55 +0100

    add pixel font and subtext on home/påmelding page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#161E38] flex justify-center items-center">
-      <div className="w-full max-w-4xl px-4">
-        <Image
-          src="/images/BytefestText.svg"
-          alt="BytefestText"
-          width={960}
-          height={480}
-        />
+    <div className="min-h-screen bg-[#161E38] flex pt-25">
+      <div className="w-full max-w-7xl mx-auto px-4 lg:px-18">
+        <div className="text-left">
+          <Image
+            src="/images/BytefestText.svg"
+            alt="BytefestText"
+            width={960}
+            height={480}
+          />
+        </div>
+        <div className="text-left argent text-white">
+          <a>OISAUDJFOPSJDFGPOAISDFJGPAOFIDGJSPAODFIGJPSODFIUGJSPODITFHGUIOJ</a>
+        </div>
       </div>
     </div>
   );
