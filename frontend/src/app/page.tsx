3b988523a7e40bb166c80c90a4a123dfd6f97177
GRIMSTVEIT Arild <arild.grimstveit@soprasteria.com>
2025-02-20 14:22:46 +0100

    add iceland font for date + time text on hompage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,12 +8,16 @@ export default function Home() {
           <Image
             src="/images/BytefestText.svg"
             alt="BytefestText"
-            width={960}
-            height={480}
+            width={940}
+            height={160}
           />
         </div>
-        <div className="text-left argent text-white">
-          <a>OISAUDJFOPSJDFGPOAISDFJGPAOFIDGJSPAODFIGJPSODFIUGJSPODITFHGUIOJ</a>
+        <div className="text-left argent text-white pt-10 text-4xl">
+          <a>Mikrokonferansen som dypdykker ned i systemutvikling</a>
+        </div>
+        <div className="text-left iceland text-white pt-10 text-5xl">
+          <a>5. Januar 2025</a>
+          <a className="text-2xl pl-20">16.00 - 21.00</a>
         </div>
       </div>
     </div>
