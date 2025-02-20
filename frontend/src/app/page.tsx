43d73957abdb6f714786fe88252faa6aee00de4a
GRIMSTVEIT Arild <arild.grimstveit@soprasteria.com>
2025-02-20 10:55:23 +0100

    add BYTEFEST text art to homepage/påmelding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#161E38] flex justify-center items-center">
       <div className="w-full max-w-4xl px-4">
-        {/* Content goes here (if needed) */}
+        <Image
+          src="/images/BytefestText.svg"
+          alt="BytefestText"
+          width={960}
+          height={480}
+        />
       </div>
     </div>
   );
