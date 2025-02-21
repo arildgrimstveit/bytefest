24b8ade8240bfed2a2a8fa6de160d197f570144d
GRIMSTVEIT Arild <arild.grimstveit@soprasteria.com>
2025-02-21 15:59:11 +0100

    add text to bytefest page

--- frontend/src/app/bytefest/page.tsx
+++ frontend/src/app/bytefest/page.tsx
@@ -4,7 +4,7 @@ export default function Bytefest() {
   return (
     <div className="min-h-screen bg-[#161E38]">
       {/* Use items-center for mobile and switch on medium screens if needed */}
-      <div className="flex flex-col lg:flex-row items-center lg:items-end justify-center pt-10 lg:pt-15 space-y-8 lg:space-y-0 lg:space-x-12">
+      <div className="flex flex-col lg:flex-row items-center lg:items-end justify-center pt-10 lg:pt-20 space-y-8 lg:space-y-0 lg:space-x-12">
         
         {/* Left section: Year and Fish together */}
         <div className="flex items-center justify-center space-x-4">
@@ -38,6 +38,23 @@ export default function Bytefest() {
           </a>
         </div>
       </div>
+
+
+      <div className="px-15 md:px-40 xl:px-80 text-white pt-10 lg:pt-20">
+          <a className="text-2xl lg:text-3xl iceland">Hva er bytefest?</a>
+          <br /> <br />
+          <a className="text-xl">Bytefest er en miktokonferanse med fokus på fag og mye gøy!
+            Den foregår på Sopra Sterias kontorer rundt omkring i landet - noen live, og noen digitalt.
+            <br /> <br />
+            I fjor var det åtte fysiske lokasjoner spredt rundt i landet,
+            og nesten 300 deltakere deltok på minikonferansen,
+            som ble holdt av og for utviklermiljøet i Sopra Steria.
+            Noen av temaene deltakerne kunne bryne seg på i fjor var Kubernetes og pipelines,
+            kodekvalitet, hvordan lage egen GPT,
+            kryssplatform mobilutvikling med React Native og .NET MAUI med mer. </a>
+
+        </div>
+
     </div>
   );
 }
