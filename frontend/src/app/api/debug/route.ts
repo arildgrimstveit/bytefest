fb149036e4600f084786f16e6d77a32413ee9420
Arild Grimstveit <arild@grimstveit.no>
2025-03-19 19:31:42 +0100

    remove unused files

--- frontend/src/app/api/debug/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    projectId: process.env.SANITY_PROJECT_ID || "Not found",
-    dataset: process.env.SANITY_DATASET || "Not found",
-  });
-}
